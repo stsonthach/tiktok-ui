685647b0d871475a68c440dbb907b141da10d664
“Your <you@example.com>
2023-01-23 22:05:42 +0700

    Dựng khung Layout mặc định

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,17 @@
+import classNames from 'classnames/bind'; // chổ này là đc import thư viện vào nên mới dùng đc thư viện đó là: npm i classnames
+import styles from './Header.module.scss';
+
+const cx = classNames.bind(styles);
+
 function Header() {
-    return <h2>Header</h2>;
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                {/* logo */}
+                {/* search */}
+            </div>
+        </header>
+    );
 }
 
 export default Header;
